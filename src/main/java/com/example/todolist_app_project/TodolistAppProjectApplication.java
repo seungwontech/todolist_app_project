2abb81974e74402ff75958159eef9b04b9a61653
seungwon <swlee173@gmail.com>
2024-07-13 16:37:37 +0900

    init

---
+++ src/main/java/com/example/todolist_app_project/TodolistAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolist_app_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistAppProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/todolist_app_project/TodolistAppProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.todolist_app_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistAppProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
