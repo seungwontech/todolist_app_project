e3da5fbcaccfe0c1a972336b593e40f358f125d8
seungwon <swlee173@gmail.com>
2024-07-17 16:03:40 +0900

    리액트 조회, 생성, 수정, 삭제 기능 및 화면 개발 및 스프링 부트 연동 (+CORS 설정)

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,25 +1,110 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from "react";
 import './App.css';
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const baseUrl = "http://localhost:8080";
+    const [todoList, setTodoList] = useState([]);
+    const [input, setInput] = useState("");
+
+    useEffect(() => {
+        getTodoList();
+    }, []);
+
+    async function getTodoList() {
+        await axios
+            .get(baseUrl + "/todo")
+            .then((response) => {
+                setTodoList(response.data);
+            })
+            .catch((error) => {
+                console.error(error)
+            })
+    }
+
+    function createTodo(e) {
+        e.preventDefault();
+        const createTodo = async () => {
+            await axios
+                .post(baseUrl + "/todo", {
+                    todoName: input
+                })
+                .then((response) => {
+                    setInput("");
+                    getTodoList();
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+        createTodo();
+    }
+
+    function updateTodo(id) {
+        const updateTodo = async () => {
+            await axios
+                .put(baseUrl + "/todo/" + id, {})
+                .then((response) => {
+                    setTodoList(
+                        todoList.map((todo) => todo.id === id ? {...todo, completed: !todo.completed} : todo)
+                    );
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+        updateTodo();
+    }
+
+    function deleteTodo(id) {
+        const updateTodo = async () => {
+            await axios
+                .delete(baseUrl + "/todo/" + id, {})
+                .then((response) => {
+                    setTodoList(
+                        todoList.filter((todo) => todo.id !== id)
+                    );
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        }
+        updateTodo();
+    }
+
+    function changeText(e) {
+        e.preventDefault();
+        setInput(e.target.value)
+    }
+
+    return (
+        <div className="App">
+            <h1>TODO LIST</h1>
+            <form onSubmit={createTodo}>
+                Todo &nbsp;
+                <input type="text" required={true} value={input} onChange={changeText}/>
+                <input type="submit" value="Create"/>
+            </form>
+            {
+                todoList
+                    ? todoList.map((todo) => {
+                        return (
+                            <div className="todoList" key={todo.id}>
+                                <h3>
+                                    <label className={todo.completed ? "completed" : null}
+                                           onClick={() => updateTodo(todo.id)}>
+                                        {todo.todoName}
+                                    </label>
+                                    <label onClick={() => delete(todo.id)}>&nbsp;&nbsp;&nbsp;❌</label>
+                                </h3>
+                            </div>
+                        )
+                    })
+                    : null
+            }
+        </div>
+    );
 }
 
 export default App;
